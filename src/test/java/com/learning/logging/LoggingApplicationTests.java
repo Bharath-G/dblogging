9ef2532dded9ab1ac0316e035ef27c769cf83fbd
Bharath-G <bharath247g@gmail.com>
2026-04-25 16:50:24 +0530

    initial commit

---
+++ src/test/java/com/learning/logging/LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
