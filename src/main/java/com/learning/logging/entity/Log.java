94e7add8229c208a6caeefd64d99f7ee2016881d
Bharath-G <bharath247g@gmail.com>
2026-05-01 21:39:13 +0530

    deleted tests updated lombok plugin

--- src/main/java/com/learning/logging/entity/Log.java
+++ src/main/java/com/learning/logging/entity/Log.java
@@ -1,7 +1,9 @@
 package com.learning.logging.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
